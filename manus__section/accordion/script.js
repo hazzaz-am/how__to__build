af38bb6e9ef9cd06e3b3324c83e40f2d21e03c3d
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-07-16 14:25:23 +0600

    accrodion design completed

---
+++ manus__section/accordion/script.js
@@ -0,0 +1,15 @@
+const accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+	accordions[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+
+		let panel = this.nextElementSibling;
+
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
